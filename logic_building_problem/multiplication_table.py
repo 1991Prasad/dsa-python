43394229371b061565681e4c921375948e646551
PrasadKharade <prasad1kharade@gmail.com>
2026-06-12 12:03:22 +0530

    Solved table multiplication and sum of natural numbers

---
+++ logic_building_problem/multiplication_table.py
@@ -0,0 +1,8 @@
+n = 5
+
+def multiplication_table(n):
+    for i in range(1,11):
+        print("%d * %d = %d" % (n,i,i*n))
+
+
+multiplication_table(n)
---
+++ logic_building_problem/sum_of_natural_number.py
@@ -0,0 +1,12 @@
+n = 5
+
+def sum_of_natural_number(n):
+    sum = 0
+    for i in range(1,n+1):
+        sum = sum + i
+       
+    print(sum)   
+
+
+
+sum_of_natural_number(n)        
